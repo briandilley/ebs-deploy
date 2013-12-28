27775ac73fdd053a6ecdc911f0b0b8c13e875598
Brian Dilley <brian.dilley@gmail.com>
2013-12-27 23:09:47 -0800

    bumped version number to 1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 
     # general meta
     name='ebs-deploy',
-    version='1.2',
+    version='1.3',
     author='Brian C. Dilley',
     author_email='brian.dilley@gmail.com',
     description='Python based command line tools for managing Amazon Elastic Beanstalk applications.',
