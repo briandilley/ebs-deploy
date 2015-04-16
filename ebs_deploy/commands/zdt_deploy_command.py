f7ccda9837f12470a9559d2a601c1820390e0827
Brian Dilley <brian.dilley@gmail.com>
2015-04-15 17:59:33 -0700

    tier_name, not tier_type

--- ebs_deploy/commands/zdt_deploy_command.py
+++ ebs_deploy/commands/zdt_deploy_command.py
@@ -29,11 +29,11 @@ def execute(helper, config, args):
     cname_prefix = env_config.get('cname_prefix', None)
 
     # no zdt for anything but web server
-    tier_type = env_config.get('tier_type', 'WebServer')
-    if tier_type != 'WebServer':
+    tier_name = env_config.get('tier_name', 'WebServer')
+    if tier_name != 'WebServer':
         raise Exception(
             "Only able to do zero downtime deployments for "
-            "WebServer tiers, can't do them for %s" % (tier_type, ))
+            "WebServer tiers, can't do them for %s" % (tier_name, ))
 
     # find existing environment name
     old_env_name = helper.environment_name_for_cname(cname_prefix)
@@ -79,8 +79,8 @@ def execute(helper, config, args):
                               description=env_config.get('description', None),
                               option_settings=option_settings,
                               version_label=version_label,
-                              tier_name=env_config.get('tier_name'),
-                              tier_type=tier_type,
+                              tier_name=tier_name,
+                              tier_type=env_config.get('tier_type'),
                               tier_version=env_config.get('tier_version'))
     helper.wait_for_environments(new_env_name, status='Ready', health='Green', include_deleted=False)
 
