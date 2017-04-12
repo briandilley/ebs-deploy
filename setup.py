cf3a59ee6c35c71b9bfda37e0a003d9a5a1d62fe
Pairing Machine 2 <pairing.machine.2@metabrite.com>
2017-04-11 17:33:42 -0700

    Bump to 1.9.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 
     # general meta
     name='ebs-deploy',
-    version='1.9.11',
+    version='1.9.12',
     author='Brian C. Dilley',
     author_email='brian.dilley@gmail.com',
     description='Python based command line tools for managing '
